8481e86228f4f7430963cbfb17a4432d489be4e7
MockCanine <2286596153@qq.com>
2026-03-26 16:39:18 +0800

    test the outOfBound and test open and isOpen can correctly modify openState array

--- sp26/proj3/src/Percolation.java
+++ sp26/proj3/src/Percolation.java
@@ -1,23 +1,32 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    // TODO: Add any necessary instance variables.
+    private final WeightedQuickUnionUF sites;
+    private final boolean[] openState, fullState;
+    private final int size;
 
     public Percolation(int N) {
-        // TODO: Fill in this constructor.
+        if (N <= 0) {
+            throw new IllegalArgumentException("input size should be positive");
+        }
+        sites = new WeightedQuickUnionUF(N);
+        openState = new boolean[N * N];
+        fullState = new boolean[N * N];
+        size = N;
     }
 
     public void open(int row, int col) {
-        // TODO: Fill in this method.
+        outOfBound(row, col);
+        openState[matrix2Array(row, col)] = true;
     }
 
     public boolean isOpen(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        outOfBound(row, col);
+        return openState[matrix2Array(row, col)];
     }
 
     public boolean isFull(int row, int col) {
-        // TODO: Fill in this method.
+        outOfBound(row, col);
         return false;
     }
 
@@ -31,7 +40,28 @@ public class Percolation {
         return false;
     }
 
-    // TODO: Add any useful helper methods (we highly recommend this!).
-    // TODO: Remove all TODO comments before submitting.
+    private void outOfBound(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new IndexOutOfBoundsException(String.format("(%s, %s) is out of bound size: %s",
+                      row, col, this.size));
+        }
+    }
 
+    private int matrix2Array(int row, int col) {
+        return row * size + col;
+    }
+
+    private boolean bornFull(int row) {
+        return row == 0;
+    }
+
+    private boolean rootFull(int row, int col) {
+        int idx = matrix2Array(row, col);
+        int root = sites.find(idx);
+        return fullState[root];
+    }
+
+    static void main() {
+        Percolation p = new Percolation(5);
+    }
 }
