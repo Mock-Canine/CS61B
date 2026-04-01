cebc5300461943e24666d59e28ba1808ce9adcdd
MockCanine <2286596153@qq.com>
2026-04-01 08:57:06 +0800

    begin to do fa25 hw9

---
+++ fa25/hw09/src/BSTMap.java
@@ -0,0 +1,116 @@
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+
+    private class BSTNode {
+        private K key;
+        private V value;
+        private BSTNode left;
+        private BSTNode right;
+
+        BSTNode(K k, V v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private BSTNode root;
+    private int size;
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map already contains the specified key, replaces the key's mapping
+     * with the value specified.
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    public void put(K key, V value) {
+        root = putHelper(key, value, root);
+    }
+
+    private BSTNode putHelper(K key, V value, BSTNode node) {
+        if (node == null) {
+            this.size++;
+            return new BSTNode(key, value);
+        }
+        if (node.key.equals(key)) {
+            node.value = value;
+        } else if (node.key.compareTo(key) > 0){
+            node.left = putHelper(key, value, node.left);
+        } else {
+            node.right = putHelper(key, value, node.right);
+        }
+        return node;
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     *
+     * @param key
+     */
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    /**
+     * Returns whether this map contains a mapping for the specified key.
+     *
+     * @param key
+     */
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    /**
+     * Returns the number of key-value mappings in this map.
+     */
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Removes every mapping from this map.
+     */
+    @Override
+    public void clear() {
+
+    }
+
+    /**
+     * Returns a Set view of the keys contained in this map. Not required for Lab 7.
+     * If you don't implement this, throw an UnsupportedOperationException.
+     */
+    @Override
+    public Set<K> keySet() {
+        return Set.of();
+    }
+
+    /**
+     * Removes the mapping for the specified key from this map if present,
+     * or null if there is no such mapping.
+     * Not required for Lab 7. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     *
+     * @param key
+     */
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+}
