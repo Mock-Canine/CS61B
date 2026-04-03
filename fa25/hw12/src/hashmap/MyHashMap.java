1b66d5557eeb4ae3d64dda548fee029e6fe603bd
MockCanine <2286596153@qq.com>
2026-04-03 16:13:52 +0800

    finish non-optional implementation of hashmap

--- fa25/hw12/src/hashmap/MyHashMap.java
+++ fa25/hw12/src/hashmap/MyHashMap.java
@@ -1,6 +1,9 @@
 package hashmap;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
 
 /**
  *  A hash table-backed Map implementation.
@@ -9,7 +12,6 @@ import java.util.Collection;
  *  @author YOUR NAME HERE
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
-
     /**
      * Protected helper class to store key/value pairs
      * The protected qualifier allows subclass access
@@ -22,16 +24,48 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
             key = k;
             value = v;
         }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj instanceof MyHashMap<?, ?>.Node other) {
+                return other.key.equals(key);
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return key.hashCode();
+        }
     }
 
     /* Instance Variables */
+    private int capacity;
+    private final double loadFactor;
     private Collection<Node>[] buckets;
-    // You should probably define some more!
+
+    // # total elements
+    private int size = 0;
+    private int initialCapacity = 16;
+    private final int resizeFactor = 2;
+    private final double defaultloadFacotr = 0.75;
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        capacity = initialCapacity;
+        loadFactor = defaultloadFacotr;
+        buckets = initTable(capacity);
+    }
 
-    public MyHashMap(int initialCapacity) { }
+    public MyHashMap(int initialCapacity) {
+        capacity = initialCapacity;
+        loadFactor = defaultloadFacotr;
+        buckets = initTable(capacity);
+        this.initialCapacity = initialCapacity;
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialCapacity.
@@ -40,7 +74,23 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialCapacity initial size of backing array
      * @param loadFactor maximum load factor
      */
-    public MyHashMap(int initialCapacity, double loadFactor) { }
+    public MyHashMap(int initialCapacity, double loadFactor) {
+        capacity = initialCapacity;
+        this.loadFactor = loadFactor;
+        buckets = initTable(capacity);
+        this.initialCapacity = initialCapacity;
+    }
+
+    /**
+     * Init an empty hash table with empty buckets
+     */
+    private Collection<Node>[] initTable(int capacity) {
+        Collection<Node>[] table = new Collection[capacity];
+        for (int i = 0; i < capacity; i++) {
+            table[i] = createBucket();
+        }
+        return table;
+    }
 
     /**
      * Returns a data structure to be a hash table bucket
@@ -63,11 +113,118 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
-        // TODO: Fill in this method.
+        return new LinkedList<>();
+    }
+
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map already contains the specified key, replaces the key's mapping
+     * with the value specified.
+     */
+    @Override
+    public void put(K key, V value) {
+        double factor = (double) size / capacity;
+        if (factor >= loadFactor) {
+            resize();
+        }
+        Node node = new Node(key, value);
+        int idx = hashIdx(node);
+        if (!buckets[idx].remove(node)) {
+            size++;
+        }
+        buckets[idx].add(node);
+    }
+
+    private void resize() {
+        capacity *= resizeFactor;
+        Collection<Node>[] newBuckets = initTable(capacity);
+        for (Collection<Node> bucket : buckets) {
+            for (Node n : bucket) {
+                newBuckets[hashIdx(n)].add(n);
+            }
+        }
+        buckets = newBuckets;
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     */
+    @Override
+    public V get(K key) {
+        Node tmp = new Node(key, null);
+        for (Node n : buckets[hashIdx(tmp)]) {
+            if (n.equals(tmp)) {
+                return n.value;
+            }
+        }
         return null;
     }
 
-    // TODO: Implement the methods of the Map61B Interface below
-    // Your code won't compile until you do so!
+    /**
+     * Returns whether this map contains a mapping for the specified key.
+     */
+    @Override
+    public boolean containsKey(K key) {
+        Node tmp = new Node(key, null);
+        return buckets[hashIdx(tmp)].contains(tmp);
+    }
 
+    /**
+     * Returns the number of key-value mappings in this map.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Removes every mapping from this map.
+     */
+    @Override
+    public void clear() {
+        capacity = initialCapacity;
+        buckets = initTable(initialCapacity);
+        size = 0;
+    }
+
+    /**
+     * Returns a Set view of the keys contained in this map. Not required for this lab.
+     * If you don't implement this, throw an UnsupportedOperationException.
+     */
+    @Override
+    public Set<K> keySet() {
+        return Set.of();
+    }
+
+    /**
+     * Removes the mapping for the specified key from this map if present,
+     * or null if there is no such mapping.
+     * Not required for this lab. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     *
+     * @param key
+     */
+    @Override
+    public V remove(K key) {
+        size--;
+        return null;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+
+    /**
+     * Calculate the entry for a node
+     */
+    private int hashIdx(Node node) {
+        return Math.floorMod(node.hashCode(), capacity);
+    }
 }
